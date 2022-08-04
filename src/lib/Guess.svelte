da2bebbcc6c800a03b2ebf681da8a294fbddaf16
Tom Howland <tomhowland9@gmail.com>
2022-08-04 11:10:45 +0100

    Format guesses

--- src/lib/Guess.svelte
+++ src/lib/Guess.svelte
@@ -1,14 +1,31 @@
 <script lang="ts">
+  import Cross from './Cross.svg.svelte';
+  import EmptyBox from './EmptyBox.svg.svelte';
+
   import type { IGuess } from './types/IGuess';
 
   export let guess: IGuess;
+
+  const colors = {
+    grey: '#999',
+    warning: 'yellow',
+    negative: 'red',
+  };
 </script>
 
 <div class="box">
   {#if guess.type === 'skipped'}
-    [] - skipped
+    <div class="icon">
+      <EmptyBox color={colors.grey} />
+    </div>
+    <div>SKIPPED</div>
   {:else if guess.type === 'guessed'}
-    <div>{JSON.stringify(guess)}</div>
+    <div class="icon">
+      <Cross color={guess.isCorrectArtist ? colors.warning : colors.negative} />
+    </div>
+    <div>{guess.artists}</div>
+    <div>-</div>
+    <div>{guess.name}</div>
   {/if}
 </div>
 
@@ -16,5 +33,13 @@
   .box {
     border: solid 1px #999;
     height: 32px;
+    display: flex;
+    align-items: center;
+  }
+  .icon {
+    display: flex;
+
+    justify-content: center;
+    width: 30px;
   }
 </style>
