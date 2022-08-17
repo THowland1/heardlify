81b56bb40e5ca980a4fd37abbfd10ebcf841c0b7
Tom Howland <tomhowland9@gmail.com>
2022-08-17 16:52:13 +0100

    Cache getting of spotify token

--- netlify/utils/get-spotify-token.ts
+++ netlify/utils/get-spotify-token.ts
@@ -8,7 +8,43 @@ type BadToken = {
   error: string;
   error_description: string;
 };
+const tokenCache = {
+  value: null as { token: Token; expiresAt: number } | null,
+};
+
+const MAX_EXECUTION_TIME_IN_MS = 10 * 1000;
+
 export async function getSpotifyToken(): Promise<Token> {
+  const fromCache = getSpotifyTokenFromCache();
+  if (fromCache) {
+    console.log('Using token from cache');
+    return fromCache;
+  }
+  const fresh = await getFreshSpotifyToken();
+  setSpotifyTokenInCache(fresh);
+  console.log('Using fresh token');
+  return fresh;
+}
+
+function setSpotifyTokenInCache(token: Token) {
+  return (tokenCache.value = {
+    token,
+    expiresAt: new Date().valueOf() + token.expires_in * 1000,
+  });
+}
+function getSpotifyTokenFromCache() {
+  const cachedToken = tokenCache.value;
+  if (!cachedToken) {
+    return null;
+  }
+  if (new Date().valueOf() > cachedToken.expiresAt - MAX_EXECUTION_TIME_IN_MS) {
+    tokenCache.value = null;
+    return null;
+  }
+  return cachedToken.token;
+}
+
+export async function getFreshSpotifyToken(): Promise<Token> {
   var client_id = process.env.SPOTIFY_CLIENT_ID;
   var client_secret = process.env.SPOTIFY_CLIENT_SECRET;
 
