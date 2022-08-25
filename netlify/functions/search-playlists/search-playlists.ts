5a6266710bf7e979177dca2655a88039d3b99cfb
Tom Howland <tomhowland9@gmail.com>
2022-08-25 17:39:00 +0100

    Allow searching by querying specific ID

--- netlify/functions/search-playlists/search-playlists.ts
+++ netlify/functions/search-playlists/search-playlists.ts
@@ -1,17 +1,79 @@
 import { Handler } from '@netlify/functions';
+import { getSpotifyPlaylist } from '../../utils/get-spotify-playlist';
 import { getSpotifyToken } from '../../utils/get-spotify-token';
 import { searchSpotifyPlaylists } from '../../utils/search-spotify-playlists';
 
-export const handler: Handler = async (event, context) => {
-  const q = event.queryStringParameters['q'];
-  const authToken = await getSpotifyToken();
-  const results = await searchSpotifyPlaylists(authToken.access_token, q);
+export type IPlaylistSummary = {
+	id: string;
+	images: { url: string }[];
+	name: string;
+	description: string;
+	owner: {
+		display_name: string;
+	};
+};
+export type ISearchPlaylistsResponse = {
+	playlists: {
+		items: IPlaylistSummary[];
+	};
+};
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(results, null, 2),
-    headers: {
-      'Access-Control-Allow-Origin': '*', // Allow from anywhere
-    },
-  };
+function mapSpotifyObjectToDto(item: SpotifyApi.PlaylistObjectSimplified): IPlaylistSummary {
+	return {
+		id: item.id,
+		description: item.description ?? '',
+		images: item.images.map((image) => ({ url: image.url })),
+		name: item.name,
+		owner: {
+			display_name: item.owner.display_name ?? ''
+		}
+	};
+}
+
+function isSpotifyId(value: string) {
+	const spotifyIdRegexp = new RegExp(
+		'^[0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz]{22}$'
+	);
+	return spotifyIdRegexp.test(value);
+}
+
+export const handler: Handler = async (event) => {
+	const q = event.queryStringParameters?.['q'];
+
+	if (!q) {
+		return {
+			statusCode: 400,
+			body: JSON.stringify({ error: { status: 400, message: 'No search query' } }, null, 2),
+			headers: {
+				'Access-Control-Allow-Origin': '*' // Allow from anywhere
+			}
+		};
+	}
+
+	const authToken = await getSpotifyToken();
+
+	let results: ISearchPlaylistsResponse;
+	if (isSpotifyId(q)) {
+		const item = await getSpotifyPlaylist(q, authToken.access_token);
+		results = {
+			playlists: {
+				items: [mapSpotifyObjectToDto(item)]
+			}
+		};
+	} else {
+		const searchResult = await searchSpotifyPlaylists(authToken.access_token, q);
+		results = {
+			playlists: {
+				items: searchResult.playlists.items.map(mapSpotifyObjectToDto)
+			}
+		};
+	}
+
+	return {
+		statusCode: 200,
+		body: JSON.stringify(results, null, 2),
+		headers: {
+			'Access-Control-Allow-Origin': '*' // Allow from anywhere
+		}
+	};
 };
