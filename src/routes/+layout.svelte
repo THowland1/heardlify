7e0e08a31dbc2489da698ca9dfe8f6444ee06a0b
Tom Howland <tomhowland9@gmail.com>
2022-08-26 07:43:14 +0100

    Do blurring page transition

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,30 @@
 <script lang="ts">
 	import '../app.css';
-
 	import { QueryClient, QueryClientProvider } from '@sveltestack/svelte-query';
+	import { navigating } from '$app/stores';
+
 	const queryClient = new QueryClient();
 </script>
 
 <QueryClientProvider client={queryClient}>
-	<slot />
+	<div class="fill" class:navigating={Boolean($navigating)}>
+		<slot />
+	</div>
 </QueryClientProvider>
+
+<style>
+	.fill {
+		position: absolute;
+		left: 0;
+		right: 0;
+		top: 0;
+		bottom: 0;
+		transition: all 0.6s ease-in-out;
+		filter: blur(0px);
+		background-color: transparent;
+	}
+	.fill.navigating {
+		background-color: #0009;
+		filter: blur(20px);
+	}
+</style>
