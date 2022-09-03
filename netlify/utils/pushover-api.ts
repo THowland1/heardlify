8ab5ce61238479f8448c40fcc132fac0caf26d4b
Tom Howland <tomhowland9@gmail.com>
2022-09-03 16:39:59 +0100

    Pause pushover API for now

--- netlify/utils/pushover-api.ts
+++ netlify/utils/pushover-api.ts
@@ -1,6 +1,7 @@
 import fetch from 'node-fetch';
 
 const trySendNotification = async (message: string) => {
+	return Promise.resolve();
 	try {
 		await fetch('https://api.pushover.net/1/messages.json', {
 			method: 'POST',
