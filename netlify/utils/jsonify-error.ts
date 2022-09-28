0238b80e8bfd9a53274ddf7f9463f6d7c2a09b8b
Tom Howland <tomhowland9@gmail.com>
2022-09-28 16:15:18 +0100

    Remove netlify functions

--- netlify/utils/jsonify-error.ts
+++
@@ -1,7 +0,0 @@
-export default function (err: unknown): Record<string, unknown> {
-	try {
-		return JSON.parse(JSON.stringify(err, Object.getOwnPropertyNames(err)));
-	} catch (error) {
-		return { jsonifyerrormessage: 'jsonify-error failed' };
-	}
-}
--- netlify/utils/mongodb-api/index.ts
+++
@@ -1,5 +0,0 @@
-import feedback from './feedback';
-import logs from './logs';
-import results from './results';
-
-export default { logs, results, feedback };
