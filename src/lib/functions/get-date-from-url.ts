96518cadecd4234d2ce9d67b46b25deff6e42afc
Tom Howland <tomhowland9@gmail.com>
2022-09-28 16:35:35 +0100

    Remove ui related files from the base directory

--- src/lib/functions/get-date-from-url.ts
+++
@@ -1,6 +0,0 @@
-export function getDateFromURL(url: URL) {
-	const value = url.searchParams.get('date');
-	if (!value) return new Date();
-	const dateValue = Date.parse(value);
-	return isNaN(dateValue) ? new Date() : new Date(dateValue);
-}
